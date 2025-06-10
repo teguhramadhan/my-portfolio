99db2a84eb30a96758b784cddb0be90fc4810eff
Teguh Ramadhan <teguhrmdhn@gmail.com>
2025-06-10 11:54:10 +0700

    add vercel analytics

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,9 @@ import HeroSection from "../components/HeroSection";
 // import ContactSection from "../components/ContactSection";
 import SplashCursor from "@/blocks/SplashCursor";
 
+// Analytics Vercel
+import { Analytics } from "@vercel/analytics/next";
+
 // Karena komponen ini akan dirender di sisi server secara default,
 // dan tidak menggunakan hooks state secara langsung di level ini,
 // kita tidak perlu 'use client'. Navbar akan menangani 'use client' sendiri.
@@ -15,6 +18,7 @@ import SplashCursor from "@/blocks/SplashCursor";
 const Home: React.FC = () => {
   return (
     <div className="min-h-screen bg-darkBg text-darkText font-sans">
+      <Analytics />
       <SplashCursor />
       <HeroSection />
       {/* <AboutSection /> */}
